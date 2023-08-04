6ee8393bc5b3f336a9011d91a676d642e37035f2
ytb <ytb664@gmail.com>
2023-08-05 04:44:41 +0700

    Init TA Booking System

---
+++ src/main/java/id/ac/ui/cs/exercise0/Exercise0Application.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.exercise0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise0Application.class, args);
+    }
+
+}
